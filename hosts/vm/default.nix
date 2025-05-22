a87347a2ca5ab99382538dce07b185c11bcce40a
philogicae <38438271+philogicae@users.noreply.github.com>
2025-05-22 16:20:30 +0300

    Remove resolution

--- hosts/vm/default.nix
+++ hosts/vm/default.nix
@@ -8,9 +8,6 @@
   # QEMU guest agent
   services.qemuGuest.enable = true;
 
-  # Set desired screen resolution for the VM via kernel parameters
-  boot.kernelParams = [ "video=2048x1152" ];
-
   # allow local remote access to make it easier to toy around with the system
   services.openssh = {
     enable = true;
